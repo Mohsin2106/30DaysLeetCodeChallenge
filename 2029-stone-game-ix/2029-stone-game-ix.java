5f2dc69eb48b26b6f916371383ba20c65f7b37df
Mohsin Quaisar <mohsinquaisar123@gmail.com>
2026-08-16 13:41:32 +0530

    Time: 3 ms (100%), Space: 114.7 MB (64.86%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -4,9 +4,11 @@ class Solution {
         for(int stone : stones){
             count[stone % 3]++;
         }
+        // check for remainder 0 stone is even 
         if(count[0] % 2 ==0){
             return count[1] > 0 && count[2] >0;
         }
+        // check for remainder 0 stone is odd
         return Math.abs(count[1] - count[2] ) > 2;
     }
 }
